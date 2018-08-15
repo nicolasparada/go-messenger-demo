52b92bec63cb4cd6a2d3da591d4ba1678a6f6962
Nicolás Parada <parada.nicolas@outlook.com>
2018-08-15 12:35:41 -0300

    Use sync.Map in the MessageBus

--- messagebus.go
+++ messagebus.go
@@ -4,8 +4,7 @@ import "sync"
 
 // MessageBus to dispatch data.
 type MessageBus struct {
-	μ       sync.Mutex
-	Clients map[MessageClient]struct{}
+	Clients sync.Map
 }
 
 // MessageClient to register in the MessageBus and receive new messages.
@@ -15,25 +14,22 @@ type MessageClient struct {
 }
 
 func (bus *MessageBus) register(ch chan Message, userID string) func() {
-	client := MessageClient{ch, userID}
-
-	bus.μ.Lock()
-	defer bus.μ.Unlock()
-	bus.Clients[client] = struct{}{}
-
+	client := &MessageClient{Ch: ch, UserID: userID}
+	bus.Clients.Store(client, nil)
 	return func() {
-		bus.μ.Lock()
-		defer bus.μ.Unlock()
-		delete(bus.Clients, client)
+		bus.Clients.Delete(client)
 	}
 }
 
 func (bus *MessageBus) send(message Message) {
-	bus.μ.Lock()
-	defer bus.μ.Unlock()
-	for client := range bus.Clients {
+	bus.Clients.Range(func(key, _ interface{}) bool {
+		client, ok := key.(*MessageClient)
+		if !ok {
+			return false
+		}
 		if client.UserID == message.ReceiverID {
 			client.Ch <- message
 		}
-	}
+		return true
+	})
 }
