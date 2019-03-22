0048fd1c2ee55de8663991f8ef84ab083b823fd5
Nicolás Parada <parada.nicolas@outlook.com>
2019-03-22 15:39:49 -0300

    updated client side router

--- static/main.js
+++ static/main.js
@@ -1,13 +1,16 @@
-import Router from 'https://unpkg.com/@nicolasparada/router@0.6.0/router.js';
+import { createRouter } from 'https://unpkg.com/@nicolasparada/router@0.8.0/router.js';
 import { guard } from './auth.js';
 import { importWithCache } from './dynamic-import.js';
 
-const router = new Router()
-router.handle('/', guard(view('home'), view('access')))
-router.handle('/callback', view('callback'))
-router.handle(/^\/conversations\/([^\/]+)$/, guard(view('conversation'), view('access')))
-router.handle(/^\//, view('not-found'))
-router.install(render)
+const viewAccess = location.hostname === 'localhost' ? view('dev-access') : view('access')
+
+const r = createRouter()
+r.route('/', guard(view('home'), viewAccess))
+r.route('/callback', view('callback'))
+r.route(/^\/conversations\/([^\/]+)$/, guard(view('conversation'), viewAccess))
+r.route(/^\//, view('not-found'))
+r.subscribe(render)
+r.install()
 
 function view(pageName) {
     return (...args) => importWithCache(`/pages/${pageName}-page.js`)
